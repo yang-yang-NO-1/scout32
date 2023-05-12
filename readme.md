88c20991b92874b5c3048c10428bf513bc74ea06
yang-yang-NO-1 <yclone123@gmail.com>
2023-05-12 18:33:53 +0800

    添加代码理解

---
+++ readme.md
@@ -0,0 +1,73 @@
+# pio使用对应于arduino框架的v1.0.2版本（pio中为v1.8.0），故修改为使用v1.0.6（pio版本为3.2.0），报错缺少dl_lib.h（arduino版本v1.0.2版本后取消该文件），从别处下载相关库放在include文件夹中
+
+# 该项目在scout32的基础上添加ws2812
+![](readme/20230512145133.png)
+# 网页图片base64对应，（按照app_server.cpp中出现顺序）
+1.
+![](readme/20230512160712.png)
+
+2.
+![](readme/20230512160554.png)
+# html部分代码解释
+```
+<tr><td align="center">Resolution</td><td align="center" colspan="2"><input type="range" id="framesize" min="0" max="6" value="5" onchange="try{fetch(document.location.origin+'/control?var=framesize&val='+this.value);}catch(e){}"></td></tr> 
+解释如下：
+这段代码是用HTML和JavaScript编写的，用于创建一个表格，其中有一个滑动条，可以调整分辨率的值。当滑动条的值改变时，它会尝试向服务器发送一个请求，传递framesize和val两个参数。代码的解释如下：
+
+- `<tr>`标签表示表格的一行。
+- `<td>`标签表示表格的一个单元格。
+- `align="center"`属性表示单元格的内容居中对齐。
+- `colspan="2"`属性表示单元格跨越两列。
+- `<input>`标签表示一个输入元素。
+- `type="range"`属性表示输入元素是一个滑动条。
+- `id="framesize"`属性表示输入元素的唯一标识符。
+- `min="0"`属性表示滑动条的最小值是0。
+- `max="6"`属性表示滑动条的最大值是6。
+- `value="5"`属性表示滑动条的初始值是5。
+- `onchange`属性表示当输入元素的值改变时，执行一段JavaScript代码。
+- `try{...}catch(e){}`语句表示尝试执行一段代码，如果出现错误，就忽略它。
+- `fetch(...)`函数表示向指定的URL发送一个请求。
+- `document.location.origin`表示当前网页的域名部分，例如https://www.bing.com。
+- `/control?var=framesize&val='+this.value`表示请求的路径和查询字符串，其中var和val是两个参数，分别传递framesize和滑动条的当前值。this指代输入元素本身。
+
+var fwdpress = new KeyboardEvent('keydown', {'keyCode':38, 'which':38});
+解释如下：
+这段代码是用JavaScript创建一个键盘事件的对象，表示用户按下了向上箭头键。²³
+
+- var fwdpress 是一个变量，用来存储键盘事件的对象。
+- new KeyboardEvent() 是一个构造函数，用来创建一个新的键盘事件的对象。
+- 'keydown' 是一个字符串，表示键盘事件的类型，即用户按下了一个键。
+- {'keyCode':38, 'which':38} 是一个对象，表示键盘事件的属性，其中keyCode和which都是38，表示向上箭头键的代码值。³⁴
+
+这样，当用户按下向上箭头键时，fwdpress变量就会包含一个描述这个动作的键盘事件的对象。你可以用这个对象来响应用户的输入，比如移动一个角色或者滚动一个页面。
+
+var currentcommand=0;
+    var oldcommand=0;
+    window.setInterval(function(){
+      if (((keyforward) && (keyleft)) || ((keybackward) && (keyleft)) || (keyleft)) {currentcommand = 3;} // Turn Left
+      else if (((keyforward) && (keyright)) || ((keybackward) && (keyright)) || (keyright)) {currentcommand = 4;} // Turn Right
+      else if (keyforward) {currentcommand = 1;} //Set Direction Forward
+      else if (keybackward) {currentcommand = 2;} // Set Direction Backward
+      else {currentcommand = 5;} // Stop
+      if (currentcommand != oldcommand){
+        fetch(document.location.origin+'/control?var=car&val='+currentcommand);
+        oldcommand = currentcommand;}
+    }, 100);
+
+这段代码是用JavaScript实现一个定时器，每隔100毫秒就向服务器发送一个请求，根据用户按键的情况改变小车的方向。¹²
+
+- var currentcommand 和 var oldcommand 是两个变量，用来存储当前和上一次的命令编号。
+- window.setInterval() 是一个函数，用来设置一个定时器，它接受两个参数：一个是要执行的函数，另一个是执行的时间间隔。¹
+- function(){} 是一个匿名函数，它在每次定时器触发时被调用。
+- if...else if...else 是一个条件语句，用来判断用户按下了哪些键，并根据不同的组合给currentcommand赋值。¹
+- if (currentcommand != oldcommand) 是一个判断语句，用来检查当前的命令是否和上一次的命令不同，如果不同，则执行后面的代码块。
+- fetch() 是一个函数，用来向服务器发送一个请求，它接受一个参数：一个是要请求的URL。²
+- document.location.origin 是一个属性，表示当前文档的域名和端口号。
+- '/control?var=car&val='+currentcommand 是一个字符串，表示请求的路径和查询参数，其中var=car表示要控制的是小车，val=表示要发送的命令编号。
+- oldcommand = currentcommand 是一个赋值语句，用来更新上一次的命令编号。
+
+这样，当用户按下不同的键时，currentcommand就会改变，并且每隔100毫秒就会向服务器发送一次请求，让小车根据命令改变方向。
+
+
+
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,17 @@
- /*
+/*
 Pilot Hobbies Scout32 - For ESP32
 */
 
 // Define Network SSID & Password
 // Set ap to 1 to use Scout32 as Standalone Access Point with default IP 192.168.4.1
 // Set ap to 0 to connect to a router using DHCP with hostname espressif
-bool ap = 1; 
-const char* ssid = "Scout32"; //AP Name or Router SSID
-const char* password = "12345678"; //Password. Leave blank for open network. 
+bool ap = 1;
+const char *ssid = "Scout32";      // AP Name or Router SSID
+const char *password = "12345678"; // Password. Leave blank for open network.
 
-//AP Settings
-int channel = 11; // Channel for AP Mode
-int hidden = 0; // Probably leave at zero
+// AP Settings
+int channel = 11;      // Channel for AP Mode
+int hidden = 0;        // Probably leave at zero
 int maxconnection = 1; // Only allow one at a time
 
 // Libraries. If you get errors compiling, please downgrade ESP32 by Espressif.
@@ -24,54 +24,54 @@ int maxconnection = 1; // Only allow one at a time
 #include <Adafruit_NeoPixel.h>
 
 // Camera Pin Definitions - Don't heckin' touch.
-#define PWDN_GPIO_NUM     32
-#define RESET_GPIO_NUM    -1
-#define XCLK_GPIO_NUM      0
-#define SIOD_GPIO_NUM     26
-#define SIOC_GPIO_NUM     27
-#define Y9_GPIO_NUM       35
-#define Y8_GPIO_NUM       34
-#define Y7_GPIO_NUM       39
-#define Y6_GPIO_NUM       36
-#define Y5_GPIO_NUM       21
-#define Y4_GPIO_NUM       19
-#define Y3_GPIO_NUM       18
-#define Y2_GPIO_NUM        5
-#define VSYNC_GPIO_NUM    25
-#define HREF_GPIO_NUM     23
-#define PCLK_GPIO_NUM     22
+#define PWDN_GPIO_NUM 32
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 0
+#define SIOD_GPIO_NUM 26
+#define SIOC_GPIO_NUM 27
+#define Y9_GPIO_NUM 35
+#define Y8_GPIO_NUM 34
+#define Y7_GPIO_NUM 39
+#define Y6_GPIO_NUM 36
+#define Y5_GPIO_NUM 21
+#define Y4_GPIO_NUM 19
+#define Y3_GPIO_NUM 18
+#define Y2_GPIO_NUM 5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM 23
+#define PCLK_GPIO_NUM 22
 
 // Webserver / Controls Function
 void startCameraServer();
 
-//Define Servos
+// Define Servos
 const int ServoPinL = 13; // Left Servo assigned to GPIO 13
-const int ServoPinR = 14;  // Right Servo assigned to GPIO 14
-//Define ws2812b
-#define PIN            12
-#define NUMPIXELS      3
+const int ServoPinR = 14; // Right Servo assigned to GPIO 14
+// Define ws2812b
+#define PIN 12
+#define NUMPIXELS 3
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-long randNumber[]={127,58,200,127,127,250,100,250,250,40,180};
+long randNumber[] = {127, 58, 200, 127, 127, 250, 100, 250, 250, 40, 180};
 char j;
 
 void initServo() // Here we setup PWM, and attach it to physical pins.
 {
-  ledcSetup(3, 50, 16); // 50 hz PWM, 16-bit resolution. Servo uses 1ms - 2ms pulse width as a fraction of 20ms period.
-  ledcSetup(4, 50, 16); // 50 hz PWM, 16-bit resolution. Pulse width is represented as integer fraction of 65536 ie 2ms = 6553.
+  ledcSetup(3, 50, 16);        // 50 hz PWM, 16-bit resolution. Servo uses 1ms - 2ms pulse width as a fraction of 20ms period.
+  ledcSetup(4, 50, 16);        // 50 hz PWM, 16-bit resolution. Pulse width is represented as integer fraction of 65536 ie 2ms = 6553.
   ledcAttachPin(ServoPinL, 3); // Attach PWM 3 to GPIO 12
   ledcAttachPin(ServoPinR, 4); // Attach PWM 4 to GPIO 13
 }
 
-void setup() 
+void setup()
 {
   WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); // Prevent brownouts by silencing them. You probably want to keep this.
-  
+
   Serial.begin(115200);
   Serial.setDebugOutput(true);
   Serial.println();
   pixels.begin();
   randomSeed(analogRead(33));
-// Camera Configuration - Again, don't touch.
+  // Camera Configuration - Again, don't touch.
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
   config.ledc_timer = LEDC_TIMER_0;
@@ -93,95 +93,104 @@ void setup()
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG;
-  //init with high specs to pre-allocate larger buffers
-  if(psramFound()){
+  // init with high specs to pre-allocate larger buffers
+  if (psramFound())
+  {
     config.frame_size = FRAMESIZE_QVGA;
     config.jpeg_quality = 10;
     config.fb_count = 2;
-  } else {
+  }
+  else
+  {
     config.frame_size = FRAMESIZE_QVGA;
     config.jpeg_quality = 12;
     config.fb_count = 1;
   }
   // camera init
   esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
+  if (err != ESP_OK)
+  {
     Serial.printf("Camera init failed with error 0x%x", err);
     return;
   }
-  //drop down frame size for higher initial frame rate
-  sensor_t * s = esp_camera_sensor_get();
+  // drop down frame size for higher initial frame rate
+  sensor_t *s = esp_camera_sensor_get();
   s->set_framesize(s, FRAMESIZE_QVGA);
   s->set_vflip(s, 1);
   s->set_hmirror(s, 1);
 
-  
   // Initialize Servos & LEDs
   initServo();
   ledcSetup(7, 5000, 8);
-  ledcAttachPin(4, 7);  //GPIO 4 is LED
-  pinMode(33, OUTPUT);  //GPIO 33 is LED
+  ledcAttachPin(4, 7); // GPIO 4 is LED
+  pinMode(33, OUTPUT); // GPIO 33 is LED
   digitalWrite(33, 0);
-  //ledcSetup(5, 5000, 8);
-  ledcAttachPin(2, 7);  //GPIO 2 is LED
+  // ledcSetup(5, 5000, 8);
+  ledcAttachPin(2, 7); // GPIO 2 is LED
 
- if(!ap){
-  // Connect to Router
-  Serial.println("ssid: " + (String)ssid);
-  Serial.println("password: " + (String)password);
-  Serial.println("WiFi is Client Scout32");
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.print("Camera Ready! Use 'http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("' to connect");
- } else {
-  // Setup Access Point
-  Serial.println("ssid: " + (String)ssid);
-  Serial.println("password: " + (String)password);
-  Serial.println("WiFi is Standalone Scout32");
-  WiFi.mode(WIFI_AP);
-  WiFi.softAP(ssid,password,channel,hidden,maxconnection);
-  Serial.print("Camera Ready! Use 'http://");
-  Serial.print(WiFi.softAPIP());
-  Serial.println("' to connect");
-}
-
-  //Flash LED as ready indicator
-  for (int i=0;i<5;i++) 
+  if (!ap)
+  {
+    // Connect to Router
+    Serial.println("ssid: " + (String)ssid);
+    Serial.println("password: " + (String)password);
+    Serial.println("WiFi is Client Scout32");
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
     {
-      ledcWrite(7,0);  // flash led
-      delay(200);
-      ledcWrite(7,10);
-      delay(200);    
-    } 
-  ledcDetachPin(4);      
-  //TURE OFF LED
-  digitalWrite(33, 1);
-  //Start Webserver
-  startCameraServer();
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.print("Camera Ready! Use 'http://");
+    Serial.print(WiFi.localIP());
+    Serial.println("' to connect");
+  }
+  else
+  {
+    // Setup Access Point
+    Serial.println("ssid: " + (String)ssid);
+    Serial.println("password: " + (String)password);
+    Serial.println("WiFi is Standalone Scout32");
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(ssid, password, channel, hidden, maxconnection);
+    Serial.print("Camera Ready! Use 'http://");
+    Serial.print(WiFi.softAPIP());
+    Serial.println("' to connect");
+  }
 
+  // Flash LED as ready indicator
+  for (int i = 0; i < 5; i++)
+  {
+    ledcWrite(7, 0); // flash led
+    delay(200);
+    ledcWrite(7, 10);
+    delay(200);
+  }
+  ledcDetachPin(4);
+  // TURE OFF LED
+  digitalWrite(33, 1);
+  // Start Webserver
+  startCameraServer();
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
   delay(1000);
-  Serial.printf("RSSi: %ld dBm\n",WiFi.RSSI());
-  pixels.clear(); 
+  Serial.printf("RSSi: %ld dBm\n", WiFi.RSSI());
+  pixels.clear();
   pixels.setBrightness(randNumber[j]);
-  for(int i=0; i<NUMPIXELS; i++){
-    pixels.setPixelColor(i, pixels.Color(randNumber[j], randNumber[j+1], randNumber[j+2]));
-    pixels.show(); 
+  for (int i = 0; i < NUMPIXELS; i++)
+  {
+    pixels.setPixelColor(i, pixels.Color(randNumber[j], randNumber[j + 1], randNumber[j + 2]));
+    pixels.show();
   }
-  if(j==10){
-    j=0;
+  if (j == 10)
+  {
+    j = 0;
   }
-  else{
+  else
+  {
     j++;
   }
-  
 }
